c5bd98a0d67e24f95ff5bee51fade82d20a5e2ea
Gaurang Kudale <gaurangkudale@yahoo.com>
2025-07-26 14:00:17 +0530

    Fix command execution by uncommenting the Run function and updating import path in main.go

--- cactl/main.go
+++ cactl/main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/gaurangkudale/cloudadvisor-cli/cmd"
+import "github.com/gaurangkudale/cloudadvisor-cli/cactl/cmd"
 
 func main() {
 	cmd.Execute()
